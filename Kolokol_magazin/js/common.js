a211638c15627af667c7d1d00d86a878f05d7538
Александр Горбачёв < >
2017-07-22 17:47:32 +0300

    add fyles

--- Kolokol_magazin/js/common.js
+++ Kolokol_magazin/js/common.js
@@ -6,6 +6,44 @@ $(document).ready(function() {
     });
     
     
+    //разворачивание/сворачивание окна 
+    
+$("#query_1").click(function(){
+    $("#ans_1__text").slideToggle();
+});   
+    
+$("#query_2").click(function(){
+    $("#ans_2__text").slideToggle();
+});
+    
+$("#query_3").click(function(){
+    $("#ans_3__text").slideToggle();
+});
+    
+$("#query_4").click(function(){
+    $("#ans_4__text").slideToggle();
+});
+    
+$("#query_5").click(function(){
+    $("#ans_5__text").slideToggle();
+});
+    
+$("#query_6").click(function(){
+    $("#ans_6__text").slideToggle();
+});
+    
+$("#query_7").click(function(){
+    $("#ans_7__text").slideToggle();
+});
+    
+$("#query_8").click(function(){
+    $("#ans_8__text").slideToggle();
+});
+    
+    
+    
+    
+    
     $('.center').slick({
   centerMode: true,
   centerPadding: '60px',
@@ -30,5 +68,6 @@ $(document).ready(function() {
       }
     }
   ]
-})
+});
+    
 });
